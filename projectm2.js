d0ea72052246dfe4c8af9b134f96b57587434958
Andrey Verkhovyh <kot3.kot3.a@gmail.com>
2022-04-15 01:48:34 +0700

    Add task button functional added

--- projectm2.js
+++ projectm2.js
@@ -26,4 +26,35 @@ function sortUp () {
     sortButton.removeEventListener(`click`, sortUp);
     sortButton.addEventListener(`click`, sortDown);
 };
-let addTaskInput = document.querySelector(`inputAddTask`);
+
+let addTaskInput = document.querySelector(`.addTaskButton`);
+addTaskInput.addEventListener(`click`, addTask);
+let idCounter = 0;
+function addTask(e) {
+    let taskInput = document.querySelector(`.inputAddTask`);
+    if (taskInput.value.replace(/\s/g,"") == "" || taskInput.value === null || taskInput.value === undefined) {
+        document.querySelector(`.error`).style.display = `block`;
+        setInterval(() =>{ 
+            document.querySelector(`.error`).style.display = `none`;
+        }, 5000 );
+    } else {
+        arrOfTasks.push({
+            id: idCounter,
+            taskName: taskInput.value
+        });
+        let newTask = document.createElement(`div`);
+        let divTasks = document.querySelector(`.tasks`);
+        let xDiv = document.createElement(`div`);
+        let taskSpan = document.createElement(`span`);
+        taskSpan.innerText = taskInput.value;
+        newTask.append(taskSpan);
+        newTask.classList.add(`task`);
+        newTask.id = idCounter;
+        idCounter++;
+        divTasks.append(newTask);
+        xDiv.classList.add(`X`);
+        xDiv.innerText = `x`;
+        newTask.append(xDiv);
+        divTasks.style.display = `block`;
+    }
+}
